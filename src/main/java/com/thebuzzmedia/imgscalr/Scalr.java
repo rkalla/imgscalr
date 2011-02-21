250de5bac816a08ff605a7bb5ee340aab2260d7f
Riyad Kalla <software@thebuzzmedia.com>
2011-02-21 11:07:15 -0700

    Updated copyright in license header.

--- src/main/java/com/thebuzzmedia/imgscalr/Scalr.java
+++ src/main/java/com/thebuzzmedia/imgscalr/Scalr.java
@@ -1,5 +1,5 @@
 /**   
- * Copyright 2010 The Buzz Media, LLC
+ * Copyright 2011 The Buzz Media, LLC
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
