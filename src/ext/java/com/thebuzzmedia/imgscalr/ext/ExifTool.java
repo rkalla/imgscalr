bdbe182f9c486db9623dda915b381c5f58850265
Riyad Kalla <software@thebuzzmedia.com>
2011-08-14 18:10:53 -0700

    Major improvements.
    Much more robust logic added to ExifTool where it will actually check
    for the installation first THEN verify the version requirement if
    stay_open functionality is requested.
    
    Added a lot of logging messages to make tracking more robust.
    
    Improved all the parsing logic to honor the EOLs that ExifTool uses
    (platform DEPENDENT).
    
    Tested on Linux to ensure it all works.
    
    Benchmarked to ensure there were no performance or memory leaks.

--- src/ext/java/com/thebuzzmedia/imgscalr/ext/ExifTool.java
+++ src/ext/java/com/thebuzzmedia/imgscalr/ext/ExifTool.java
@@ -1,5 +1,6 @@
 package com.thebuzzmedia.imgscalr.ext;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,6 +10,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+/*
+ * TODO: Is there a way to discover the PID of the ExifTool process this class
+ * spawns so it can keep track of it in Close and issue a taskkill (win) or
+ * kill -9 (linux/mac/unix) in an attempt to clean up stray processes if necessary?
+ */
+
+/*
+ * TODO: Add a few static checkExifTool methods to this class that this class
+ * can both use and external processes (e.g. imgscalr Rotation.AUTO) can use to
+ * verify the installation of ExifTool before trying to use this library and 
+ * having something explode.
+ */
 
 /**
  * Class used to provide a Java-like wrapper to Phil Harvey's excellent <a
@@ -21,14 +36,34 @@ import java.util.StringTokenizer;
  * <p/>
  * This class is an attempt at making ExifTool integration into a Java app
  * seamless and feel just like using a Java library.
- * <h3>Performance</h3>
- * Extra care is taken to ensure minimal to no garbage creation or unnecessary
- * CPU overhead while communicating with the external process.
+ * <h3>ExifTool -stay_open Support</h3>
+ * ExifTool <a href=
+ * "http://u88.n24.queensu.ca/exiftool/forum/index.php/topic,1402.msg12933.html#msg12933"
+ * >8.36</a> added a new persistent-process feature that allows ExifTool to stay
+ * running in a daemon mode and continue accepting commands via a file or stdin.
+ * This new mode is controlled via the <code>-stay_open True/False</code>
+ * command line argument.
  * <p/>
- * As is typical with external process communication in Java, a litany of
- * temporary {@link String} objects are created then disposed or even multiplied
- * via a {@link String#split(String)} operation just to process the content
- * coming back from the external process.
+ * If a version of ExifTool earlier than 8.36 is used, the
+ * <code>-stay_open</code> command line argument is ignored and the code path
+ * inside this tool written to take advantage of it will hang the executing
+ * thread waiting for a reply from an incompatible ExifTool process.
+ * <p/>
+ * To protect against this nasty side effect, this class, when configured to use
+ * the <code>stayOpen</code> mode, will make an attempt at verifying the version
+ * of ExifTool <em>before</em> the first call to the external tool. Once the
+ * version has been verified, the check will not be performed again for the life
+ * of the instance. If the version is confirmed to be lower than the required
+ * ExifTool version to support that feature, an exception is thrown to notify
+ * the caller.
+ * <h3>Performance</h3>
+ * Extra care is taken to ensure minimal object creation or unnecessary CPU
+ * overhead while communicating with the external process.
+ * <p/>
+ * {@link Pattern}s used to split the responses from the process are explicitly
+ * compiled and reused, string concatenation is minimized, Tag name lookup is
+ * done via a <code>static final</code> {@link Map} shared by all instances and
+ * so on.
  * <p/>
  * Additionally, extra care is taken to utilize the most optimal code paths when
  * initiating and using the external process, for example, the
@@ -121,7 +156,7 @@ public class ExifTool {
 	 */
 	public static final String LOG_PREFIX = "[imgscalr.ext.exiftool] ";
 
-	private static final int DEFAULT_BUFFER_SIZE = 1024;
+	private static final Pattern TAG_VALUE_PATTERN = Pattern.compile(": ");
 
 	/**
 	 * Enum used to specify the different supported tags that can be extracted
@@ -140,19 +175,34 @@ public class ExifTool {
 	 * @since 3.2
 	 */
 	public enum Tag {
-		ISO("-ISO"), APERTURE("-ApertureValue"), SHUTTER_SPEED(
-				"-ShutterSpeedValue"), FOCAL_LENGTH("-FocalLength"), EXPOSURE_COMPENSATION(
-				"-ExposureCompensation"), EXPOSURE_PROGRAM("-ExposureProgram"), ORIENTATION(
-				"-Orientation"), MAKE("-Make"), MODEL("-Model"), TITLE(
-				"-XPTitle"), AUTHOR("-XPAuthor"), SUBJECT("-XPSubject"), KEYWORDS(
-				"-XPKeywords"), COMMENT("-XPComment"), RATING("-Rating"), RATING_PERCENT(
-				"-RatingPercent"), DATE_TIME_ORIGINAL("-DateTimeOriginal"), GPS_LATITUDE(
-				"-GPSLatitude"), GPS_LATITUDE_REF("-GPSLatitudeRef"), GPS_LONGITUDE(
-				"-GPSLongitude"), GPS_LONGITUDE_REF("-GPSLongitudeRef"), GPS_ALTITUDE(
-				"-GPSAltitude"), GPS_ALTITUDE_REF("-GPSAltitudeRef"), GPS_SPEED(
-				"-GPSSpeed"), GPS_SPEED_REF("-GPSSpeedRef"), GPS_PROCESS_METHOD(
-				"-GPSProcessingMethod"), GPS_BEARING("-GPSDestBearing"), GPS_BEARING_REF(
-				"-GPSDestBearingRef");
+		ISO("ISO"), APERTURE("ApertureValue"), SHUTTER_SPEED(
+				"ShutterSpeedValue"), FOCAL_LENGTH("FocalLength"), EXPOSURE_COMPENSATION(
+				"ExposureCompensation"), EXPOSURE_PROGRAM("ExposureProgram"), ORIENTATION(
+				"Orientation"), MAKE("Make"), MODEL("Model"), TITLE("XPTitle"), AUTHOR(
+				"XPAuthor"), SUBJECT("XPSubject"), KEYWORDS("XPKeywords"), COMMENT(
+				"XPComment"), RATING("Rating"), RATING_PERCENT("RatingPercent"), DATE_TIME_ORIGINAL(
+				"DateTimeOriginal"), GPS_LATITUDE("GPSLatitude"), GPS_LATITUDE_REF(
+				"GPSLatitudeRef"), GPS_LONGITUDE("GPSLongitude"), GPS_LONGITUDE_REF(
+				"GPSLongitudeRef"), GPS_ALTITUDE("GPSAltitude"), GPS_ALTITUDE_REF(
+				"GPSAltitudeRef"), GPS_SPEED("GPSSpeed"), GPS_SPEED_REF(
+				"GPSSpeedRef"), GPS_PROCESS_METHOD("GPSProcessingMethod"), GPS_BEARING(
+				"GPSDestBearing"), GPS_BEARING_REF("GPSDestBearingRef");
+
+		private static final Map<String, Tag> LOOKUP_MAP;
+
+		static {
+			Tag[] values = Tag.values();
+			LOOKUP_MAP = new HashMap<String, ExifTool.Tag>(values.length * 3);
+
+			for (int i = 0; i < values.length; i++) {
+				Tag tag = values[i];
+				LOOKUP_MAP.put(tag.name, tag);
+			}
+		}
+
+		public static Tag forName(String name) {
+			return LOOKUP_MAP.get(name);
+		}
 
 		private String name;
 
@@ -162,11 +212,11 @@ public class ExifTool {
 	}
 
 	private boolean stayOpen;
+	private boolean stayOpenSupportVerified;
 
-	private char[] buffer;
 	private List<String> args;
 
-	private InputStreamReader reader;
+	private BufferedReader reader;
 	private OutputStreamWriter writer;
 
 	public ExifTool() {
@@ -175,9 +225,9 @@ public class ExifTool {
 
 	public ExifTool(boolean stayOpen) {
 		this.stayOpen = stayOpen;
+		this.stayOpenSupportVerified = false;
 
 		args = new ArrayList<String>(64);
-		buffer = new char[DEFAULT_BUFFER_SIZE];
 	}
 
 	public boolean isOpen() {
@@ -189,23 +239,33 @@ public class ExifTool {
 		if (!stayOpen)
 			return;
 
-		try {
-			log("Attempting to close persistent ExifTool process, issuing -stay_open\nFalse\n...");
+		/*
+		 * If ExifTool was used in stayOpen mode but getImageMeta was never
+		 * called then the streams were never initialized and there is nothing
+		 * to shut down or destroy, otherwise we need to close down all the
+		 * resources in use.
+		 */
+		if (reader == null && writer == null) {
+			log("\tThis ExifTool instance was never used so no external process or streams were ever created (nothing to clean up, we can just exit).");
+		} else {
+			try {
+				log("\tAttempting to close ExifTool daemon process, issuing '-stay_open\\nFalse\\n' command...");
 
-			// Tell the ExifTool process to exit.
-			writer.write("-stay_open\nFalse\n");
-			writer.flush();
+				// Tell the ExifTool process to exit.
+				writer.write("-stay_open\nFalse\n");
+				writer.flush();
 
-			log("\tSuccessful");
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			closeStreams();
+				log("\t\tSuccessful");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				closeStreams();
+			}
 		}
 
 		// Update the open state.
 		stayOpen = false;
-		log("Persistent ExifTool process successfully shut down.");
+		log("\tExifTool daemon process successfully terminated.");
 	}
 
 	public Map<Tag, String> getImageMeta(File image, Tag... tags)
@@ -217,14 +277,14 @@ public class ExifTool {
 			throw new SecurityException(
 					"Unable to read the given image ["
 							+ image.getAbsolutePath()
-							+ "], ensure that the file permissions are correct and that the executing process is able to read the given image.");
+							+ "], ensure that the image exists at the given path and that the executing Java process has permissions to read it.");
 
 		long startTime = System.currentTimeMillis();
 		Map<Tag, String> resultMap = new HashMap<ExifTool.Tag, String>(
 				tags.length * 3);
 
 		if (DEBUG)
-			log("Quering %d tags for values from image: %s", tags.length,
+			log("Querying %d tags from image: %s", tags.length,
 					image.getAbsolutePath());
 
 		// Ensure there is work to do first.
@@ -233,12 +293,16 @@ public class ExifTool {
 			args.clear();
 
 			if (stayOpen) {
+				log("\tUsing ExifTool in daemon mode (-stay_open True)...");
+
 				/*
 				 * If this is our first time calling getImageMeta with a
 				 * stayOpen connection, set up the persistent process and run it
 				 * so it is ready to receive commands from us.
 				 */
 				if (reader == null && writer == null) {
+					log("\tStarting daemon ExifTool process and creating read/write streams (this only happens once)...");
+
 					args.add(EXIF_TOOL_PATH);
 					args.add("-stay_open");
 					args.add("True");
@@ -249,11 +313,14 @@ public class ExifTool {
 					runProcess(args);
 				}
 
+				log("\tStreaming arguments to ExifTool process...");
+
 				writer.write("-n\n"); // numeric output
 				writer.write("-S\n"); // compact output
 				writer.write("-fast\n"); // do not search to EOF
 
 				for (int i = 0; i < tags.length; i++) {
+					writer.write('-');
 					writer.write(tags[i].name);
 					writer.write("\n");
 				}
@@ -261,10 +328,14 @@ public class ExifTool {
 				writer.write(image.getAbsolutePath());
 				writer.write("\n");
 
+				log("\tExecuting ExifTool...");
+
 				// Run ExifTool on our file with all the given arguments.
 				writer.write("-execute\n");
 				writer.flush();
 			} else {
+				log("\tUsing ExifTool in non-daemon mode (-stay_open False)...");
+
 				/*
 				 * Since we are not using a stayOpen process, we need to setup
 				 * the execution arguments completely each time.
@@ -276,7 +347,7 @@ public class ExifTool {
 				args.add("-fast"); // do not search to EOF
 
 				for (int i = 0; i < tags.length; i++)
-					args.add(tags[i].name);
+					args.add("-" + tags[i].name);
 
 				args.add(image.getAbsolutePath());
 
@@ -284,47 +355,33 @@ public class ExifTool {
 				runProcess(args);
 			}
 
-			/*
-			 * Regardless of how we executed ExifTool above, reading and parsing
-			 * the reply is the same; so begin reading back the reply.
-			 */
-			int charsRead = 0;
-			int charsKept = 0;
+			log("\tReading response back from ExifTool...");
 
-			/*
-			 * Keep filling the buffer (without overwriting any kept content)
-			 * until we hit the end of stream.
-			 */
-			while ((charsRead = reader.read(buffer, charsKept, buffer.length
-					- charsKept)) != -1) {
-				// Calculate the length of all the content in the buffer.
-				int length = charsKept + charsRead;
+			String line = null;
 
-				System.out.println(new String(buffer, 0, length));
+			while ((line = reader.readLine()) != null) {
+				String[] pair = TAG_VALUE_PATTERN.split(line);
+
+				if (pair != null && pair.length == 2) {
+					// Determine the tag represented by this value.
+					Tag tag = Tag.forName(pair[0]);
+
+					// Store the tag and its associated value in the result map.
+					resultMap.put(tag, pair[1]);
+					log("\t\tRead Tag [name=%s, value=%s]", tag.name, pair[1]);
+				}
 
 				/*
-				 * TODO: This end logic has to be tested on Mac and Linux -- on
-				 * Windows ET is writing out \r\n before and after {ready} --
-				 * need to figure out if this is ET's behavior OR if it is just
-				 * writing out the platform's default newline.
+				 * When using a persistent ExifTool process, it terminates its
+				 * output to us with a {ready} clause, we need to look for it
+				 * and break from this loop when we see it otherwise this
+				 * process will hang.
 				 */
-
-				/*
-				 * If we are using a persistent ExifTool process, ET lets us
-				 * know that it is done responding by writing out a \n{ready} to
-				 * the output stream which we need to check for, because EOS is
-				 * never hit and we will block forever waiting for it.
-				 */
-				if (stayOpen && buffer[length - 3] == '}'
-						&& buffer[length - 4] == 'y'
-						&& buffer[length - 5] == 'd'
-						&& buffer[length - 6] == 'a'
-						&& buffer[length - 7] == 'e'
-						&& buffer[length - 8] == 'r'
-						&& buffer[length - 9] == '{'
-						&& buffer[length - 10] == '\n')
+				if (stayOpen && line.equals("{ready}"))
 					break;
 			}
+
+			log("\tDone reading ExifTool response.");
 
 			/*
 			 * If we are not using a persistent ExifTool process, then after
@@ -376,21 +433,69 @@ public class ExifTool {
 	}
 
 	protected void runProcess(List<String> args) throws RuntimeException {
+		/*
+		 * Ensure we have verified stayOpen support with the installed ExifTool
+		 * version, otherwise our code path for that mode will hang.
+		 */
+		if (stayOpen && !stayOpenSupportVerified)
+			checkExifToolVersion();
+
 		Process proc = null;
 
 		try {
+			log("\tAttempting to start process: %s", args);
+
 			proc = new ProcessBuilder(args).start();
+
+			log("\t\tSuccessful");
 		} catch (Exception e) {
 			throw new RuntimeException(
 					"Unable to start the ExifTool process using the following execution arguments "
 							+ args
-							+ ". Ensure ExifTool is installed, runs correctly and is a relatively current release.",
+							+ ". Ensure ExifTool is installed and executable using the command '"
+							+ EXIF_TOOL_PATH
+							+ "'. The path used to executed ExifTool can be adjusted using the 'imgscalr.ext.exiftool.path' system property.",
 					e);
 		}
 
+		log("\tSetting up Read/Write streams to process...");
+
 		// Setup read/write streams to the new process.
-		reader = new InputStreamReader(proc.getInputStream());
+		reader = new BufferedReader(
+				new InputStreamReader(proc.getInputStream()));
 		writer = new OutputStreamWriter(proc.getOutputStream());
+
+		log("\t\tSuccessful");
+	}
+
+	protected void checkExifToolVersion() throws RuntimeException {
+		try {
+			log("\tVerifiying installed ExifTool supports daemon mode (this check is only done once)...");
+
+			Process proc = new ProcessBuilder(EXIF_TOOL_PATH, "-ver").start();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					proc.getInputStream()));
+			String ver = reader.readLine();
+			reader.close();
+
+			if (ver.length() >= 3 && ver.compareTo("8.36") >= 0) {
+				stayOpenSupportVerified = true;
+
+				log("\t\tRequired ExifTool version CONFIRMED, needed version 8.36 or higher and found version %s.",
+						ver);
+			} else {
+				String message = "Required ExifTool version NOT FOUND. Needed version 8.36 or higher but found version "
+						+ ver + " instead.";
+				log("\t\t%s", message);
+				throw new RuntimeException(message);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(
+					"Unable to start the ExifTool process in order to verify the version '"
+							+ EXIF_TOOL_PATH
+							+ " -ver'. Ensure ExifTool is installed and executable using the command path defined by the 'imgscalr.ext.exiftool.path' system property.",
+					e);
+		}
 	}
 
 	protected void closeStreams() {
