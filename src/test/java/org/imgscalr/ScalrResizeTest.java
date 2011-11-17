a67ac1fc9144695f45139f4960bc07f754d716a2
Riyad Kalla <software@thebuzzmedia.com>
2011-11-17 12:03:18 -0700

    Added test cases for all resize ops.

---
+++ src/test/java/org/imgscalr/ScalrResizeTest.java
@@ -0,0 +1,123 @@
+package org.imgscalr;
+
+import junit.framework.Assert;
+
+import org.imgscalr.Scalr.Method;
+import org.imgscalr.Scalr.Mode;
+import org.junit.Test;
+
+public class ScalrResizeTest extends AbstractScalrTest {
+	@Test
+	public void testResizeEX() {
+		try {
+			Scalr.resize(src, -1);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			Scalr.resize(src, 240, -1);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			Scalr.resize(src, (Method) null, 240);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			Scalr.resize(src, (Mode) null, 240);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			Scalr.resize(src, (Method) null, 240, 240);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			Scalr.resize(src, (Mode) null, 240, 240);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			Scalr.resize(src, null, null, 240);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+
+		try {
+			Scalr.resize(src, null, null, 240, 240);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+	}
+
+	@Test
+	public void testResizeSize() {
+		assertEquals(load("time-square-resize-320.png"), Scalr.resize(src, 320));
+	}
+
+	@Test
+	public void testResizeWH() {
+		assertEquals(load("time-square-resize-640x480.png"),
+				Scalr.resize(src, 640, 480));
+	}
+
+	@Test
+	public void testResizeSizeSpeed() {
+		assertEquals(load("time-square-resize-320-speed.png"),
+				Scalr.resize(src, Method.SPEED, 320));
+	}
+
+	@Test
+	public void testResizeWHSpeed() {
+		assertEquals(load("time-square-resize-640x480-speed.png"),
+				Scalr.resize(src, Method.SPEED, 640, 480));
+	}
+
+	@Test
+	public void testResizeSizeExact() {
+		assertEquals(load("time-square-resize-320-fit-exact.png"),
+				Scalr.resize(src, Mode.FIT_EXACT, 320));
+	}
+
+	@Test
+	public void testResizeWHExact() {
+		assertEquals(load("time-square-resize-640x640-fit-exact.png"),
+				Scalr.resize(src, Mode.FIT_EXACT, 640, 640));
+	}
+
+	@Test
+	public void testResizeSizeSpeedExact() {
+		assertEquals(load("time-square-resize-320-speed-fit-exact.png"),
+				Scalr.resize(src, Method.SPEED, Mode.FIT_EXACT, 320));
+	}
+
+	@Test
+	public void testResizeWHSpeedExact() {
+		assertEquals(load("time-square-resize-640x640-speed-fit-exact.png"),
+				Scalr.resize(src, Method.SPEED, Mode.FIT_EXACT, 640, 640));
+	}
+
+	@Test
+	public void testResizeWHSpeedExactOps() {
+		assertEquals(
+				load("time-square-resize-640x640-speed-fit-exact-ops.png"),
+				Scalr.resize(src, Method.SPEED, Mode.FIT_EXACT, 640, 640,
+						Scalr.OP_GRAYSCALE));
+	}
+}
