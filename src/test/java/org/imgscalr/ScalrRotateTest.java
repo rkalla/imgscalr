3c11c082ccd496c865159c85c2cfa6756c4889b8
Riyad Kalla <software@thebuzzmedia.com>
2011-11-17 11:10:17 -0700

    Added Scalr.rotate test.

---
+++ src/test/java/org/imgscalr/ScalrRotateTest.java
@@ -0,0 +1,47 @@
+package org.imgscalr;
+
+import org.imgscalr.Scalr.Rotation;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScalrRotateTest extends AbstractScalrTest {
+	@Test
+	public void testRotateEX() {
+		try {
+			Scalr.rotate(src, null);
+			Assert.assertTrue(false);
+		} catch (IllegalArgumentException e) {
+			Assert.assertTrue(true);
+		}
+	}
+
+	@Test
+	public void testRotate90() {
+		assertEquals(load("time-square-rotate-90.png"),
+				Scalr.rotate(load("time-square.png"), Rotation.CW_90));
+	}
+
+	@Test
+	public void testRotate180() {
+		assertEquals(load("time-square-rotate-180.png"),
+				Scalr.rotate(load("time-square.png"), Rotation.CW_180));
+	}
+
+	@Test
+	public void testRotate270() {
+		assertEquals(load("time-square-rotate-270.png"),
+				Scalr.rotate(load("time-square.png"), Rotation.CW_270));
+	}
+
+	@Test
+	public void testRotateFlipH() {
+		assertEquals(load("time-square-rotate-horz.png"),
+				Scalr.rotate(load("time-square.png"), Rotation.FLIP_HORZ));
+	}
+
+	@Test
+	public void testRotateFlipV() {
+		assertEquals(load("time-square-rotate-vert.png"),
+				Scalr.rotate(load("time-square.png"), Rotation.FLIP_VERT));
+	}
+}
